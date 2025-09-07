67afc627ba50c81ca789e29cc5fe1532c2abe06e
uday1881 <146739743+uday1881@users.noreply.github.com>
2025-09-07 13:22:58 +0000

    container config done

---
+++ 01_basics/comparison.js
@@ -0,0 +1,9 @@
+console.log("2">1);
+console.log("02">1);  /// here string is auto converted
+
+console.log(null>0); // false  
+console.log(null==0); // false   
+console.log(null>=0); // true   here comparison operator converted null to a number and taling it as 0
+
+
+// STRICT CHECK ALER!      === (checks values as well as datatype)
