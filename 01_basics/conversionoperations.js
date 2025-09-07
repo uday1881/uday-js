b5a88d0800347e8ee94a125f0ac1cea613aa456c
uday1881 <146739743+uday1881@users.noreply.github.com>
2025-09-07 12:44:57 +0000

    container config done

---
+++ 01_basics/conversionoperations.js
@@ -0,0 +1,21 @@
+let score = 33
+console.log(typeof score);
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+
+let constring = String(score)
+console.log(typeof score);
+console.log(typeof constring);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0;
+
+let nun = "33abc"
+let NuN = Number(nun);
+console.log(NuN);
+
+//1 => true;
+//"" => false;
+//"uday" => true;
